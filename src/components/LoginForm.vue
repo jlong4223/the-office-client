3364df8c5438b4e75aeeee05c029fc77d68dfcf7
Jared Long <jlong4223@gmail.com>
2021-03-24 12:27:31 -0500

    adds login and register form components

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <fieldset>
+      <legend>Login</legend>
+      <input type="text" placeholder="name" />
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <legend>Register</legend>
+    <fieldset>
+      <input type="text" />
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
