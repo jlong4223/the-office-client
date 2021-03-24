52260020803dde9f2e287438ffe5337e790d72d4
Jared Long <jlong4223@gmail.com>
2021-03-24 11:57:37 -0500

    creates landing component

---
+++ src/components/Landing.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Home</h1>
+  <h2>Michael Scott</h2>
+  <img
+    src="https://pbs.twimg.com/profile_images/1050414908762939393/UKzYsgQg.jpg"
+    alt="mScott"
+  />
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
