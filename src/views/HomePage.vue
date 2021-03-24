5a9d28f76d921232919d1c0db73c7318c4556af1
Jared Long <jlong4223@gmail.com>
2021-03-24 11:41:46 -0500

    creates two pages in views dir

---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
