b9447d40c5ce142c1b438b4f950700849ae81823
Jared Long <jlong4223@gmail.com>
2021-03-24 12:34:16 -0500

    imports forms to each view

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,10 +1,14 @@
 <template>
   <h1>Login Page</h1>
+  <LoginForm />
 </template>
 
 <script>
 import { defineComponent } from "@vue/composition-api";
+import LoginForm from "../components/LoginForm";
+
 export default defineComponent({
+  components: { LoginForm },
   setup() {},
 });
 </script>
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Register Here</h1>
+  <RegisterForm />
+</template>
+
+<script>
+import RegisterForm from "../components/RegisterForm";
+export default {
+  components: { RegisterForm },
+  setup() {},
+};
+</script>
