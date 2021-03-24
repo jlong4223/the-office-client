3e46c43b81313d11ecbb0befb81e537fffc70ae7
Jared Long <jlong4223@gmail.com>
2021-03-24 12:27:18 -0500

    updates heading

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,6 +1,5 @@
 <template>
-  <h1>Home</h1>
-  <h2>Michael Scott</h2>
+  <h1>Signup and Login to view Michael Scott Quotes</h1>
   <img
     src="https://images-na.ssl-images-amazon.com/images/I/41CVehkzm6L._AC_SY450_.jpg"
     alt="mScott"
