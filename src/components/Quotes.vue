fb2eeb9eda3eb48ff0f35f45c46e4cf2d48463d1
Jared Long <jlong4223@gmail.com>
2021-03-24 22:37:41 -0500

    fetches quote data from rails api

---
+++ src/components/Quotes.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Quotes Below</h1>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+export default {
+  name: "Quotes",
+  setup() {
+    const quotes = ref([]);
+
+    async function fetchData() {
+      const response = await fetch(`http://localhost:3000/offices`);
+      quotes.value = await response.json();
+    }
+
+    onMounted(fetchData());
+
+    return { quotes };
+  },
+};
+</script>
