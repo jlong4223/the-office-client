1fdc80c83e634e3988aee024fd12c3245a36b294
Jared Long <jlong4223@gmail.com>
2021-03-25 20:42:13 -0500

    testing process for adding user with setup

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,15 +1,61 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <legend>Register</legend>
     <fieldset>
-      <input type="text" />
+      <input type="text" placeholder="name" v-model="user.name" required />
+      <input type="email" placeholder="email" v-model="user.email" required />
+      <input
+        type="password"
+        placeholder="password"
+        v-model="user.password"
+        required
+      />
+      <button>Submit</button>
     </fieldset>
   </form>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-  // TODO this form will post to /users?
-  setup() {},
+  setup() {
+    const BASE_URL = "http://localhost:3000/users";
+    // split the below into three sepertae and change the v-model
+    const user = ref({
+      name: "",
+      email: "",
+      password: "",
+    });
+    // const email = ref("");
+
+    // const name = ref("");
+
+    // const password = ref("");
+
+    // const user = {
+    //   name: "user",
+    //   email: "user@user.com",
+    //   password: "user",
+    // };
+
+    async function registerUser(user) {
+      await fetch(BASE_URL, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+    }
+
+    async function handleSubmit() {
+      try {
+        await registerUser(user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    return { registerUser, handleSubmit, user /*email, name, password*/ };
+  },
 };
 </script>
