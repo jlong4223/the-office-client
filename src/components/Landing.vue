bceb75ca615f764a3444add8e2693b581b9837ae
Jared Long <jlong4223@gmail.com>
2021-03-27 20:59:27 -0500

    moves the get user from token out of this page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,5 @@
 <template>
   <h1>Signup and Login to view Michael Scott Quotes</h1>
-  <!-- TODO only show user if there is one -->
-  <h2>Hi {{ user }}</h2>
   <img
     src="https://images-na.ssl-images-amazon.com/images/I/41CVehkzm6L._AC_SY450_.jpg"
     alt="mScott"
@@ -9,20 +7,7 @@
 </template>
 
 <script>
-import { getUserFromToken } from "../services/TokenService";
-import { onMounted, ref } from "vue";
 export default {
-  setup() {
-    const user = ref([]);
-
-    async function setUser() {
-      const response = await getUserFromToken();
-      user.value = response;
-    }
-
-    onMounted(setUser());
-
-    return { user };
-  },
+  setup() {},
 };
 </script>
