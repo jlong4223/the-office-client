d5f4e3636ef9e01fbf406d199b3e9b5469f54d98
Jared Long <jlong4223@gmail.com>
2021-04-23 14:52:03 -0500

    adds service to get user route based on id

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,10 @@
-import { removeToken } from "./TokenService";
+import { removeToken, getUserId } from "./TokenService";
 
 /* ------- User Services --------- */
-const BASE_URL = "http://localhost:3000/users/login";
+const BASE_URL = "http://localhost:3000/users/";
 
 function loginUser(user) {
-  return fetch(BASE_URL, {
+  return fetch(BASE_URL + "login", {
     method: "post",
     headers: new Headers({
       "Content-Type": "application/json",
@@ -17,4 +17,10 @@ function logout() {
   return removeToken();
 }
 
-export { loginUser, logout };
+function getUserFavorites() {
+  return fetch(BASE_URL + `${getUserId()}`);
+}
+
+export { loginUser, logout, getUserFavorites };
+
+// /users/${id} - pass in the getuserbyid from token service
