b20c50e2137b72d06d6ff9740128e57f74acf3a3
Jared Long <jlong4223@gmail.com>
2021-04-23 14:53:17 -0500

    new component to show user favorites

---
+++ src/components/UserFavs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Favorites Page</div>
+  <div v-for="fa in fav.favorites" v-bind:key="fa.author">
+    <h1>{{ fa.quote }}</h1>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import { getUserFavorites } from "../services/UserService";
+export default {
+  setup() {
+    const fav = ref([]);
+
+    // getting all user info based on user id
+    async function getData() {
+      const data = await getUserFavorites();
+      fav.value = await data.json();
+    }
+
+    onMounted(getData());
+
+    return { fav };
+  },
+};
+</script>
+
+// TODO add styles
