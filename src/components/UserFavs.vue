aad1ec38b134f7ffde4dfa39ed1b1ee6b10d4329
Jared Long <jlong4223@gmail.com>
2021-04-28 16:20:12 -0500

    adds div with v-if to hide more elements if no favs

--- src/components/UserFavs.vue
+++ src/components/UserFavs.vue
@@ -6,31 +6,33 @@
   <div v-if="fav.favorites && !fav.favorites.length">
     <h1>You havent added any favorites yet :(</h1>
   </div>
-  <h1><strong>Favorite Michael Scott Quotes:</strong></h1>
-  <div id="quote-container">
-    <div v-for="fa in fav.favorites" v-bind:key="fa.author">
-      <!-- only showing quotes here based on MScott -->
-      <div id="quoteDiv" v-if="fa.author.includes('Michael Scott')">
-        <h1>{{ fa.quote }}</h1>
-        <h2>{{ fa.author }}</h2>
-        <button class="button is-danger" @click="handleDelete(fa.id)">
-          <i class="fas fa-trash-alt"></i>
-        </button>
+  <div v-if="fav.favorites && fav.favorites.length !== 0">
+    <h1><strong>Favorite Michael Scott Quotes:</strong></h1>
+    <div id="quote-container">
+      <div v-for="fa in fav.favorites" v-bind:key="fa.author">
+        <!-- only showing quotes here based on MScott -->
+        <div id="quoteDiv" v-if="fa.author.includes('Michael Scott')">
+          <h1>{{ fa.quote }}</h1>
+          <h2>{{ fa.author }}</h2>
+          <button class="button is-danger" @click="handleDelete(fa.id)">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-  <h1><strong>Favorite Ron Swanson Quotes:</strong></h1>
-  <div id="quote-container">
-    <div v-for="fa in fav.favorites" v-bind:key="fa.author">
-      <!-- only showing quotes here based on RSwanson -->
-      <div id="quoteDiv" v-if="fa.author === 'Ron Swanson'">
-        <!-- TODO show message if no quotes for this author -->
-        <!-- <h1 v-if="!fav.favorites.author.includes('Ron Swanson')">No Quotes Yet</h1> -->
-        <h1>{{ fa.quote }}</h1>
-        <h2>{{ fa.author }}</h2>
-        <button class="button is-danger" @click="handleDelete(fa.id)">
-          <i class="fas fa-trash-alt"></i>
-        </button>
+    <h1><strong>Favorite Ron Swanson Quotes:</strong></h1>
+    <div id="quote-container">
+      <div v-for="fa in fav.favorites" v-bind:key="fa.author">
+        <!-- only showing quotes here based on RSwanson -->
+        <div id="quoteDiv" v-if="fa.author === 'Ron Swanson'">
+          <!-- TODO show message if no quotes for this author -->
+          <!-- <h1 v-if="!fav.favorites.author.includes('Ron Swanson')">No Quotes Yet</h1> -->
+          <h1>{{ fa.quote }}</h1>
+          <h2>{{ fa.author }}</h2>
+          <button class="button is-danger" @click="handleDelete(fa.id)">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </div>
       </div>
     </div>
   </div>
