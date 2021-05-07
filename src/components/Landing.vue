240e83cf46bffca8b946945929f326b1884c44d4
Jared Long <jlong4223@gmail.com>
2021-05-06 22:49:23 -0500

    adds sample quote, brings in user data from props and styles page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,43 +1,67 @@
 <template>
-  <h1>Signup and Login to view Michael Scott or Ron Swanson Quotes</h1>
-  <div>
-    <router-link to="/quotes"
-      ><img
-        id="scott"
-        src="https://images-na.ssl-images-amazon.com/images/I/41CVehkzm6L._AC_SY450_.jpg"
-        alt="mScott"
-    /></router-link>
-    <router-link to="swanson_quotes">
-      <img
-        id="swanson"
-        src="https://i.etsystatic.com/16692478/r/il/2b4631/1416991338/il_570xN.1416991338_1ndf.jpg"
-        alt="mScott"
-    /></router-link>
+  <h1 v-if="user.name === null">
+    Signup and Login to view Michael Scott or Ron Swanson Quotes
+  </h1>
+  <div id="page">
+    <div class="img-quote">
+      <router-link to="/quotes"
+        ><img
+          id="scott"
+          src="https://images-na.ssl-images-amazon.com/images/I/41CVehkzm6L._AC_SY450_.jpg"
+          alt="mScott"
+      /></router-link>
+      <p>&ldquo;That's what she said&rdquo;</p>
+    </div>
+    <div class="img-quote">
+      <p>
+        &ldquo;Fishing relaxes me. It’s like yoga, except I still get to kill
+        something.&rdquo;
+      </p>
+      <router-link to="swanson_quotes">
+        <img
+          id="swanson"
+          src="https://i.etsystatic.com/16692478/r/il/2b4631/1416991338/il_570xN.1416991338_1ndf.jpg"
+          alt="mScott"
+      /></router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  setup() {},
+  props: {
+    user: {
+      name: String,
+    },
+  },
+  setup(context, props) {
+    // const username = context.attrs.user.name;
+    return { context, props };
+  },
 };
 </script>
 
 <style scoped>
-/* div {
-  display: flex; 
-  height: 80vh;
-  justify-content: space-between;
+#page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-} */
-
-/* img:hover {
-  transform: scale(1.2, 1.2);
-} */
-
-/* #swanson {
-  width: 630px;
+}
+.img-quote {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 75%;
+  height: 370px;
+  margin: 10px 0;
+  border-radius: 10px;
+  border: 1px solid black;
+}
+.img-quote p {
+  font-size: 20px;
 }
 #scott {
-  margin-left: 150px;
-} */
+  height: 300px;
+}
 </style>
