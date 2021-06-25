51f9976b37b00909379e3d914a6dc6e13f4b11a1
Jared Long <jlong4223@gmail.com>
2021-06-25 13:52:32 -0500

    feat: uses axios to post form data to node api

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -9,9 +9,12 @@
         alt="user"
         width="150"
       />
-      <button class="button is-info" title="Disabled button" disabled>
+      <form @submit.prevent="postUserPicData" enctype="multipart/form-data"> 
+        <input type="file" v-on:change="updatePicInfoImage" />
+      <button class="button is-info" title="Disabled button">
         Upload Profile Image
       </button>
+      </form>
       <h3>Profile Image Upload coming soon</h3>
     </div>
     <div>
@@ -25,7 +28,8 @@
 
 <script>
 import { getUserFavorites } from "../services/UserService";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
+import axios from 'axios'
 
 export default {
   setup() {
@@ -34,24 +38,60 @@ export default {
     const userName = ref();
     const createdAt = ref();
 
+    const picInfo = reactive({
+      userID: '',
+      application: 'office-client',
+      image: null
+    })
+
+    onMounted(getData());
+
     async function getData() {
       try {
         const data = await getUserFavorites();
         const userData = await data.json();
-        console.log(userData);
+        /* saving all ref values based on data from user */
         favAmount.value = userData.favorites.length;
         userName.value = userData.name;
         createdAt.value = new Date(userData.created_at);
         userInfo.value = userData;
-
-        // loading.value.status = false;
+        picInfo.userID = userData.id
       } catch (e) {
         console.log(e);
       }
     }
 
-    onMounted(getData());
-    return { favAmount, userName, userInfo, createdAt };
+    const updatePicInfoImage = (event) => {
+      picInfo.image = event.target.files[0]
+    }
+
+// TODO if they change their image, have a put request that goes through and changes their image instead of saving a new one
+// may need an function that gets the userid route from backend and if they exist, put instead of create
+    async function postUserPicData() {
+      const userPicData = new FormData()
+      /* adding all elements from picInfo to the form data and posting it */
+      userPicData.append("image", picInfo.image)
+      userPicData.append("userID", picInfo.userID)
+      userPicData.append("application", picInfo.application)
+        try{
+          axios.post("http://localhost:3001/allapps", userPicData)
+        }catch(err){
+          console.log(err)
+        }
+      }
+
+    // TODO have a function that gets the image and updates the picture
+
+
+    return { 
+      favAmount,
+      userName, 
+      userInfo, 
+      createdAt, 
+      picInfo, 
+      updatePicInfoImage, 
+      postUserPicData 
+      };
   },
 };
 </script>
